d157b4507b02fc08637d8f01c086115b587b3a0d
Kienan.Vella <technogeek123.v@gmail.com>
2021-05-15 01:32:10 -0400

    implement initial fan profiles with interpolation

--- code.py
+++ code.py
@@ -1,4 +1,4 @@
-# Kienan Vella's PicoFan
+# Kienan Vella's PicoFan V0.5
 # A digital PWM fan controller implemented in CircuitPython, interfaced over usb serial.
 # Targeted at the pi pico, but readily adapdable to other MCUs.
 
@@ -15,10 +15,18 @@ import pwmio
 # without a mux, there are 6 channels of pwm out, with 4 of them having speed sensors
 # with a mux, you can have up to 8 channels with pwm out, and all 8 of them can have speed sensors.
 
-mux = False # only set this to true if you have a 74HC4052 connected
+mux = True # only set this to true if you have a 74HC4052 connected. The MAX4692 should also be compatible, though packaging is not very accessible for most hobbyists.
 thermLabel = [ "Rad in", "Rad out" ]
-fanLabel = [ "front", "top", "bottom", "rear", "pump1", "pump2", "chipset" ]  # there should be 6 labels here, or 8 if you have a mux connected.
-ch0_profile = [ [0, 50, 60, 70, 80 ], [20, 30, 50, 70, 100] ] # fan profile - the first list is temperature in degrees celcius, the second list is fan pwm %
+fanLabel = [ "front1", "front2", "front3", "top1", "top2", "top3", "pump1", "pump2" ]  # there should be 6 labels here, or 8 if you have a mux connected.
+
+# Fan profile - the profileTemp list is temperatures in degrees celcius, the profileDC list is fan pwm %, mapped to the profileTemp list.
+# The profileSensor list ties a specific pwm channel to a specific temperature sensor, eg, if you have two loops.
+# both lists need to have the same number of elements, corresponding to the fanLabel positions above.
+# if you have 3  pin fans or non-pwm pumps attached for rpm monitoring, make sure they are at the end of the channel chain (eg, two 3 pin pumps in channel 6 and 7)
+# these profiles will do nothing for 3 pin devices - there is NO voltage control implemented as of v0.3.
+profileTemp = [ [0, 30, 40, 50, 60], [0, 30, 40, 50, 60] ]
+profileDC = [ [25, 40, 50, 70, 100], [25, 40, 50, 70, 100] ]
+profileSensor = [ 0, 1, 0, 1, 0, 1, 0, 1 ]
 
 # set up tachometer pins and flow sensor pin transition counters
 if mux == False:
@@ -32,7 +40,7 @@ elif mux == True:
     # set up two tach input channels that our 74HC4052 mux will connect to
     tach0 = countio.Counter(board.GP11)
     tach1 = countio.Counter(board.GP13)
-    # mux control pins - the 74HC4052 is a 4 pole dual throw mux, so 8 tachometers can be sampled two at a time, in 4 pairs
+    # mux control pins - the 74HC4052 is a dual pole quad throw mux, so 8 tachometers can be sampled two at a time, in 4 pairs
     muxA = digitalio.DigitalInOut(board.GP8)
     muxB = digitalio.DigitalInOut(board.GP9)
     RPM = [0, 0, 0, 0, 0, 0, 0, 0]
@@ -81,6 +89,24 @@ def temperature(r, Ro=10000.0, To=25.0, beta=3950.0):
     cel = (1.0 / cel) - 273.15   # Invert, convert to C
     return cel
 
+def Pct2val(pct):
+    import math
+    val = int(int(pct) * 65535 / 100)
+    return val
+
+def ReadProfile(fan):
+    index = 0
+    while index <= 3  # cycle through the fan profile value lists
+        if profileTemp[fan][index] => temp[0] => profileTemp[fan][index + 1]:  # compare current index to the next index
+            fromSpan = profileTemp[fan][index + 1] - profileTemp[fan][index]   # build a value map
+            toSpan = profileDC[fan][index + 1] - profileDC[fan][index]
+
+            Scaled = float(temp[ profileSensor[fan] ] - profileTemp[fan][index]) / float(fromSpan) # calculate a scale factor for PWM value
+
+            return int(profileDC[fan][index] + ( Scaled * toSpan )) # calculate and return the scaled pwm value
+
+        index += 1
+
 # main loop
 while True:
     if supervisor.runtime.serial_bytes_available:
@@ -110,7 +136,7 @@ while True:
         # set fan speed with a "%" symbol
         if inText.startswith("%"):
             pctText = inText[1:]
-            spd = int(int(pctText) * 65535 / 100)
+            spd = Pct2val(int(pcText)
 
             if ((fanNumber < 0) or (fanNumber > maxch)):
                 # when no fan selected, or selection out of range, adjust speed of all fans
@@ -135,6 +161,13 @@ while True:
             fan6.duty_cycle = fanSpeed[6]
             fan7.duty_cycle = fanSpeed[7]
 
+    for f in range(maxch):
+        try:
+            fanDC[f] = ReadProfile(f)
+            fanSpeed[f] = Pct2val(fanDC[f])
+        except:
+            False
+
     # time, flies like an arrow
     now = time.monotonic()
 
@@ -158,8 +191,8 @@ while True:
                 RPM[3] = (tach0.count * 60) # * 60 to account for two pole tach, this is a simplification of count * 120 / 2
                 RPM[7] = (tach1.count * 60) # it's possible this will have to be configurable for different fan tach types
                 # change mux for next reading
-                muxA.value = false
-                muxB.value = false
+                muxA.value = False
+                muxB.value = False
                 # reset tach counts
                 tach0.reset()
                 tach1.reset()
@@ -168,8 +201,8 @@ while True:
                 RPM[0] = (tach0.count * 60)
                 RPM[4] = (tach1.count * 60)
 
-                muxA.value = true
-                muxB.value = false
+                muxA.value = True
+                muxB.value = False
 
                 tach0.reset()
                 tach1.reset()
@@ -178,8 +211,8 @@ while True:
                 RPM[1] = (tach0.count * 60)
                 RPM[5] = (tach1.count * 60)
 
-                muxA.value = false
-                muxB.value = true
+                muxA.value = False
+                muxB.value = True
 
                 tach0.reset()
                 tach1.reset()
@@ -188,8 +221,8 @@ while True:
                 RPM[2] = (tach0.count * 60)
                 RPM[6] = (tach1.count * 60)
 
-                muxA.value = true
-                muxB.value = true
+                muxA.value = True
+                muxB.value = True
 
                 tach0.reset()
                 tach1.reset()
